f31221fe9e6bbecf8fe44d05aaa7216ad99693ca
gschudeck <gabrielschudeck@gmail.com>
2025-07-13 21:03:52 -0400

    Add files via upload

---
+++ src/ferre-pos-central/.eslintrc.js
@@ -0,0 +1,130 @@
+module.exports = {
+  env: {
+    browser: false,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    // Reglas personalizadas para el proyecto
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { 
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_' 
+    }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-arrow-callback': 'error',
+    'arrow-spacing': 'error',
+    'prefer-template': 'error',
+    'template-curly-spacing': 'error',
+    'prefer-destructuring': ['error', {
+      'array': false,
+      'object': true
+    }],
+    'no-duplicate-imports': 'error',
+    'no-useless-rename': 'error',
+    'rest-spread-spacing': 'error',
+    'prefer-spread': 'error',
+    'no-useless-computed-key': 'error',
+    'no-useless-constructor': 'error',
+    'no-dupe-class-members': 'error',
+    'no-this-before-super': 'error',
+    'constructor-super': 'error',
+    'prefer-numeric-literals': 'error',
+    'symbol-description': 'error',
+    'class-methods-use-this': 'off',
+    'consistent-return': 'error',
+    'default-case': 'error',
+    'dot-notation': 'error',
+    'eqeqeq': ['error', 'always'],
+    'guard-for-in': 'error',
+    'no-alert': 'error',
+    'no-caller': 'error',
+    'no-case-declarations': 'error',
+    'no-div-regex': 'error',
+    'no-else-return': 'error',
+    'no-empty-function': 'error',
+    'no-empty-pattern': 'error',
+    'no-eq-null': 'error',
+    'no-eval': 'error',
+    'no-extend-native': 'error',
+    'no-extra-bind': 'error',
+    'no-extra-label': 'error',
+    'no-fallthrough': 'error',
+    'no-floating-decimal': 'error',
+    'no-global-assign': 'error',
+    'no-implicit-coercion': 'error',
+    'no-implicit-globals': 'error',
+    'no-implied-eval': 'error',
+    'no-invalid-this': 'off',
+    'no-iterator': 'error',
+    'no-labels': 'error',
+    'no-lone-blocks': 'error',
+    'no-loop-func': 'error',
+    'no-magic-numbers': 'off',
+    'no-multi-spaces': 'error',
+    'no-multi-str': 'error',
+    'no-new': 'error',
+    'no-new-func': 'error',
+    'no-new-wrappers': 'error',
+    'no-octal': 'error',
+    'no-octal-escape': 'error',
+    'no-param-reassign': 'error',
+    'no-proto': 'error',
+    'no-redeclare': 'error',
+    'no-restricted-properties': 'error',
+    'no-return-assign': 'error',
+    'no-return-await': 'error',
+    'no-script-url': 'error',
+    'no-self-assign': 'error',
+    'no-self-compare': 'error',
+    'no-sequences': 'error',
+    'no-throw-literal': 'error',
+    'no-unmodified-loop-condition': 'error',
+    'no-unused-expressions': 'error',
+    'no-unused-labels': 'error',
+    'no-useless-call': 'error',
+    'no-useless-concat': 'error',
+    'no-useless-escape': 'error',
+    'no-useless-return': 'error',
+    'no-void': 'error',
+    'no-warning-comments': 'warn',
+    'no-with': 'error',
+    'prefer-promise-reject-errors': 'error',
+    'radix': 'error',
+    'require-await': 'error',
+    'vars-on-top': 'error',
+    'wrap-iife': 'error',
+    'yoda': 'error'
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/*.spec.js'],
+      env: {
+        jest: true
+      },
+      rules: {
+        'no-magic-numbers': 'off'
+      }
+    }
+  ]
+}
+
